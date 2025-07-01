8f08a3d3bcb373ec76dafd9130475179bf9fd6da
Flapchik <58729268+Flapchik@users.noreply.github.com>
2025-07-01 18:40:31 +0300

    Сделано API для кликера

---
+++ utils/api.py
@@ -0,0 +1,71 @@
+import os
+
+from aiohttp import ClientSession, BasicAuth
+
+API_URL = os.getenv("CLICKER_SERVER_API_URL")
+USERNAME = os.getenv("CLICKER_SERVER_USERNAME")
+PASSWORD = os.getenv("CLICKER_SERVER_PASSWORD")
+
+
+async def run_enter():
+    async with ClientSession() as session:
+        async with session.post(
+                f"http://{API_URL}/run-enter",
+                auth=BasicAuth(USERNAME, PASSWORD)
+        ) as response:
+            return await response.json()
+
+
+async def restore_dino(
+        steamid: str,
+        growth: float,
+        hunger: float,
+        thirst: float,
+        health: float
+):
+    data = {
+        "steamid": steamid,
+        "growth": growth,
+        "hunger": hunger,
+        "thirst": thirst,
+        "health": health
+    }
+    async with ClientSession() as session:
+        async with session.post(
+                f"http://{API_URL}/run-restore-dino",
+                json=data,
+                auth=BasicAuth(USERNAME, PASSWORD)
+        ) as response:
+            return await response.json()
+
+
+async def slay_dino(steamid: str):
+    data = {"steamid": steamid}
+    async with ClientSession() as session:
+        async with session.post(
+                f"http://{API_URL}/run-slay-dino",
+                json=data,
+                auth=BasicAuth(USERNAME, PASSWORD)
+        ) as response:
+            return await response.json()
+
+
+async def set_nutrients(
+        steamid: str,
+        prot: float,
+        carb: float,
+        lipid: float
+):
+    data = {
+        "steamid": steamid,
+        "prot": prot,
+        "carb": carb,
+        "lipid": lipid
+    }
+    async with ClientSession() as session:
+        async with session.post(
+                f"http://{API_URL}/run-set-nutrients",
+                json=data,
+                auth=BasicAuth(USERNAME, PASSWORD)
+        ) as response:
+            return await response.json()
